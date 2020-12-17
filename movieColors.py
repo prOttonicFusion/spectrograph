1ebdde2fff695e3ef32d57d30176d66ce8af84c7
Otto Lindblom <61689180+prOttonicFusion@users.noreply.github.com>
2020-12-17 11:23:25 +0200

    create basic program outline

---
+++ movieColors.py
@@ -0,0 +1,12 @@
+import sys
+import cv2
+import numpy as np
+from PIL import Image
+
+# Read video file
+
+# Check need for color correction
+
+# Parse frames to PIL images
+
+# Extract primary color
