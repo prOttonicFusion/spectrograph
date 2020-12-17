8514995e076a2d041c42ffb62b43abc9404e0870
Otto Lindblom <61689180+prOttonicFusion@users.noreply.github.com>
2020-12-17 11:30:06 +0200

    add color extraction function

--- movieColors.py
+++ movieColors.py
@@ -11,12 +11,33 @@ from PIL import Image
 
 # Extract primary color
 
+
 def main():
     img_path = sys.argv[1]
     img = Image.open(img_path)
 
     main_color = get_primary_color(img)
     print('Dominant color: ', rgbToHex(main_color))
+
+
+def get_primary_color(source_img, palette_size=32):
+    # Scale down image to conserve resources
+    img = source_img.copy()
+    img.thumbnail((100, 100))
+
+    # Reduce color palette (using k-means)
+    img_reduced = img.convert('P', palette=Image.ADAPTIVE, colors=palette_size)
+
+    # Get list of colors in image
+    palette = img_reduced.getpalette()
+
+    # Find most common color
+    color_counts = sorted(img_reduced.getcolors(), reverse=True)
+    primary_index = color_counts[0][1]
+    primary_color = palette[primary_index*3:primary_index*3+3]
+
+    return primary_color
+
 
 if __name__ == "__main__":
     main()
