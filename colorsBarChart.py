a2d7f1666b504a9c060079db6859ec4a1a160ad1
Otto Lindblom <61689180+prOttonicFusion@users.noreply.github.com>
2020-12-18 11:17:51 +0200

    allow exporting plot as png & html

--- colorsBarChart.py
+++ colorsBarChart.py
@@ -1,13 +1,12 @@
 import numpy as np
 import argparse
 from math import pi
-from bokeh.io import output_file, show
+from bokeh.io import output_file, export_png, show
 from bokeh.plotting import figure
 
 
-def main(color_data_file):
+def main(color_data_file, html, png):
     colors = np.loadtxt(color_data_file, comments="%", dtype=str)
-    print(colors[0])
     data_range = len(colors)
     data = list(range(0, data_range))
 
@@ -16,7 +15,11 @@ def main(color_data_file):
 
     fig.vbar(x=data, top=1, width=0.9, color=colors)
 
-    output_file('plot.html', title='Bokeh Plot', mode=None, root_dir=None)
+    if html != '':
+        output_file(html, mode=None, root_dir=None)
+
+    if png != '':
+        export_png(fig, filename=png)
 
     show(fig)
 
@@ -25,9 +28,13 @@ def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         'color_data_file', help='path to the data file containing a list of colors')
+    parser.add_argument('--html',
+                        help='set the output file name and save as HTML', default='')
+    parser.add_argument('--png',
+                        help='set the output file name and save as png', default='')
     args = parser.parse_args()
 
-    return [args.color_data_file]
+    return [args.color_data_file, args.html, args.png]
 
 
 if __name__ == "__main__":
