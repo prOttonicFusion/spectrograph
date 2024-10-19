198733564c023444764fba6d4b0f87e260d6be91
Otto Lindblom <otto.lindblom@koherent.io>
2024-10-19 10:45:30 -0400

    format and type hint

--- linearSpectrum.py
+++ linearSpectrum.py
@@ -2,24 +2,20 @@
 Plot a spectrum from a data file of color codes
 """
 
-__author__ = "prOttonicFusion"
-__version__ = "0.1.0"
-__license__ = "MIT"
-
 import numpy as np
 import argparse
-from math import pi
 from bokeh.io import output_file, export_png, show
 from bokeh.plotting import figure
 
 
-def main(color_data_file, html='', png='', title='', show_axes=False, show_grid=False):
-    colors = np.loadtxt(color_data_file, comments="%", dtype=str)
+def main(
+    data_file_path: str, html="", png="", title="", show_axes=False, show_grid=False
+):
+    colors = np.loadtxt(data_file_path, comments="%", dtype=str)
     data_range = len(colors)
     data = list(range(0, data_range))
 
-    fig = figure(height=250, title=title,
-                 toolbar_location=None, tools='')
+    fig = figure(height=250, title=title, toolbar_location=None, tools="")
 
     fig.vbar(x=data, top=1, width=0.9, color=colors)
 
@@ -31,10 +27,10 @@ def main(color_data_file, html='', png='', title='', show_axes=False, show_grid=
         fig.ygrid.grid_line_color = None
         fig.outline_line_color = None
 
-    if html != '':
+    if html != "":
         output_file(html, mode=None, root_dir=None)
 
-    if png != '':
+    if png != "":
         export_png(fig, filename=png)
 
     show(fig)
@@ -43,20 +39,41 @@ def main(color_data_file, html='', png='', title='', show_axes=False, show_grid=
 def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        'color_data_file', help='path to the data file containing a list of colors')
-    parser.add_argument('--html',
-                        help='set the output file name and save as HTML, default: none', default='')
-    parser.add_argument('--png',
-                        help='set the output file name and save as png, default: none', default='')
-    parser.add_argument('--title',
-                        help='set plot title, default: none', default='')
-    parser.add_argument('--show_axes',
-                        help='show plot axes, default: False', action='store_true', default=False)
-    parser.add_argument('--show_grid',
-                        help='show plot grid, default: False', action='store_true', default=False)
+        "data_file_path", help="path to the data file containing a list of colors"
+    )
+    parser.add_argument(
+        "--html",
+        help="set the output file name and save as HTML, default: none",
+        default="",
+    )
+    parser.add_argument(
+        "--png",
+        help="set the output file name and save as png, default: none",
+        default="",
+    )
+    parser.add_argument("--title", help="set plot title, default: none", default="")
+    parser.add_argument(
+        "--show_axes",
+        help="show plot axes, default: False",
+        action="store_true",
+        default=False,
+    )
+    parser.add_argument(
+        "--show_grid",
+        help="show plot grid, default: False",
+        action="store_true",
+        default=False,
+    )
     args = parser.parse_args()
 
-    return [args.color_data_file, args.html, args.png, args.title, args.show_axes, args.show_grid]
+    return [
+        args.data_file_path,
+        args.html,
+        args.png,
+        args.title,
+        args.show_axes,
+        args.show_grid,
+    ]
 
 
 if __name__ == "__main__":
--- spectrograph.py
+++ spectrograph.py
@@ -2,19 +2,20 @@
 Extract the primary colors from each frame of a video file
 """
 
-__author__ = "prOttonicFusion"
-__version__ = "0.1.0"
-__license__ = "MIT"
-
-import sys
 import cv2
 import argparse
-import numpy as np
 from PIL import Image
 
 
 def analyze_movie(
-        video_path, aspect_ratio=0, palette_size=32, frames=-1, step=1, show_frames=False, show_last_frame=False, color_format='hex'
+    video_path: str,
+    aspect_ratio: float = 0,
+    palette_size: int = 32,
+    frames: int = -1,
+    step: int = 1,
+    show_frames: bool = False,
+    show_last_frame: bool = False,
+    color_format: str = "hex",
 ):
     """Parses and prints out the primary color of every STEPth video frame
 
@@ -54,69 +55,104 @@ def analyze_movie(
                 width, height = pil_img.size
                 left = 0
                 right = width
-                content_height = 1/aspect_ratio * width
+                content_height = 1 / aspect_ratio * width
                 border = (height - content_height) * 0.5
                 top = border
                 bottom = border + content_height
                 pil_img = pil_img.crop((left, top, right, bottom))
 
             # Get primary color
-            main_color = get_primary_color(
-                pil_img, palette_size, show_img=show_frames)
+            main_color = get_primary_color(pil_img, palette_size, show_img=show_frames)
 
-            if color_format == 'hex':
+            if color_format == "hex":
                 main_color = rgbToHex(main_color)
-                
+
             print(main_color)
 
         # Attempt to read next frame
         success, image = vidcap.read()
         count += 1
 
-    if show_last_frame:
+    if show_last_frame and pil_img:
         pil_img.show()
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('video_path', help='path to the video file')
-    parser.add_argument('--aspect_ratio', '-a',
-                        help='specify the aspect ratio using the format 4:3 to crop out border, default: 0', default=0)
-    parser.add_argument('--palette_size', '-p',
-                        help='number of distinct colors in color space, default 32', type=int, default=32)
-    parser.add_argument('--frames', '-f',
-                        help='number of video frames to parse, with -1 being all, default: -1', type=int, default=-1)
-    parser.add_argument('--step', '-s',
-                        help='step size, i.e. parse every STEPth frames, default: 1', type=int, default=1)
-    parser.add_argument('--show_frames',
-                        help='show each processed frame for debugging purposes', action='store_true', default=False)
-    parser.add_argument('--show_last_frame',
-                        help='show last frame for debugging purposes', action='store_true', default=False)
-    parser.add_argument('--color_format',
-                        help='specify the color format as hex or rgb, default: hex', default='hex')
+    parser.add_argument("video_path", help="path to the video file")
+    parser.add_argument(
+        "--aspect_ratio",
+        "-a",
+        help="specify the aspect ratio using the format 4:3 to crop out border, default: 0",
+        default=0,
+    )
+    parser.add_argument(
+        "--palette_size",
+        "-p",
+        help="number of distinct colors in color space, default 32",
+        type=int,
+        default=32,
+    )
+    parser.add_argument(
+        "--frames",
+        "-f",
+        help="number of video frames to parse, with -1 being all, default: -1",
+        type=int,
+        default=-1,
+    )
+    parser.add_argument(
+        "--step",
+        "-s",
+        help="step size, i.e. parse every STEPth frames, default: 1",
+        type=int,
+        default=1,
+    )
+    parser.add_argument(
+        "--show_frames",
+        help="show each processed frame for debugging purposes",
+        action="store_true",
+        default=False,
+    )
+    parser.add_argument(
+        "--show_last_frame",
+        help="show last frame for debugging purposes",
+        action="store_true",
+        default=False,
+    )
+    parser.add_argument(
+        "--color_format",
+        help="specify the color format as hex or rgb, default: hex",
+        default="hex",
+    )
     args = parser.parse_args()
 
     if isinstance(args.aspect_ratio, str):
-        splitted = args.aspect_ratio.split(':')
+        splitted = args.aspect_ratio.split(":")
         try:
-            args.aspect_ratio = float(splitted[0])/float(splitted[1])
+            args.aspect_ratio = float(splitted[0]) / float(splitted[1])
         except:
-            raise(Exception('Unable to parse aspect ratio'))
+            raise (Exception("Unable to parse aspect ratio"))
 
     if args.show_frames and args.frames == -1:
-        input("Warning: This will open each video frame in a new window. To continue, press enter")
+        input(
+            "Warning: This will open each video frame in a new window. To continue, press enter"
+        )
 
-    return [args.video_path,
-            args.aspect_ratio,
-            args.palette_size,
-            args.frames,
-            args.step,
-            args.show_frames,
-            args.show_last_frame,
-            args.color_format]
+    return [
+        args.video_path,
+        args.aspect_ratio,
+        args.palette_size,
+        args.frames,
+        args.step,
+        args.show_frames,
+        args.show_last_frame,
+        args.color_format,
+    ]
 
 
-def get_primary_color(source_img, palette_size, show_img=False):
+def get_primary_color(
+    source_img: Image.Image, palette_size: int, show_img=False
+) -> list[int]:
     """Get the primary color of an image by scaling it down and reducing
        the color palette
 
@@ -131,7 +167,7 @@ def get_primary_color(source_img, palette_size, show_img=False):
 
     Returns
     ----------
-    primary_color : tuple
+    primary_color : list[int]
         A RGB tuple describing the frame's most common color
     """
 
@@ -140,25 +176,34 @@ def get_primary_color(source_img, palette_size, show_img=False):
     img.thumbnail((100, 100))
 
     # Reduce color palette (using k-means)
-    img_reduced = img.convert('P', palette=Image.ADAPTIVE, colors=palette_size)
+    img_reduced = img.convert("P", palette=Image.Palette.ADAPTIVE, colors=palette_size)
     if show_img:
         img_reduced.show()
 
     # Get list of colors in image
     palette = img_reduced.getpalette()
+    if not palette:
+        raise Exception("Failed to exctract image color palette")
 
     # Find most common color
-    color_counts = sorted(img_reduced.getcolors(), reverse=True)
+    colors = img_reduced.getcolors()
+    if not colors:
+        raise Exception("Failed to exctract image color list")
+
+    color_counts = sorted(colors, reverse=True)
     primary_index = color_counts[0][1]
-    primary_color = palette[primary_index*3:primary_index*3+3]
+    if not isinstance(primary_index, (int, float)):
+        raise Exception("Expected primary index to be a number for P-mode image")
+
+    primary_color = palette[primary_index * 3 : primary_index * 3 + 3]
 
     return primary_color
 
 
-def rgbToHex(rgb_color):
+def rgbToHex(rgb_color: list[int]):
     """Converts a RGB tuple to a hex color string"""
-    r, g, b = rgb_color
-    return '#{:02x}{:02x}{:02x}'.format(r, g, b)
+    r, g, b = rgb_color[0:3]
+    return "#{:02x}{:02x}{:02x}".format(r, g, b)
 
 
 if __name__ == "__main__":
